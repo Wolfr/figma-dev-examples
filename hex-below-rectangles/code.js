e23763a5a4ede533b2aa70da77024baf0b9a0ccf
Johan Ronsse <johan.ronsse@gmail.com>
2023-07-19 20:52:20 -0500

    Fine-tuning close plugin for every plugin, plus a screenshot for plugins

--- hex-below-rectangles/code.js
+++ hex-below-rectangles/code.js
@@ -1,4 +1,11 @@
-// Make sure *everyhing* is a top level frame/rect before running this.
+async function loadFonts() {
+    await Promise.all([
+        figma.loadFontAsync({
+            family: "Inter",
+            style: "Regular"
+        }),
+    ])
+}
 
 function componentToHex(c) {
     const hex = c.toString(16);
@@ -12,25 +19,30 @@ function rgbToHex(r, g, b) {
     return "#" + red + green + blue;
 }
  
-for (const node of figma.currentPage.selection) {
+loadFonts().then(() => {
+  for (const node of figma.currentPage.selection) {
     
-    const r = node.fills[0].color.r;
-    const g = node.fills[0].color.g;
-    const b = node.fills[0].color.b;
-    const hex = rgbToHex(r, g, b);
+      if (node.fills[0].color) {
+        const r = node.fills[0].color.r;
+        const g = node.fills[0].color.g;
+        const b = node.fills[0].color.b;
+        const hex = rgbToHex(r, g, b);
 
-    const height = node.absoluteBoundingBox.height;
-    const width = node.absoluteBoundingBox.width;
-    const x = node.absoluteBoundingBox.x;
-    const y = node.absoluteBoundingBox.y;
+        const height = node.absoluteBoundingBox.height;
+        const width = node.absoluteBoundingBox.width;
+        const x = node.absoluteBoundingBox.x;
+        const y = node.absoluteBoundingBox.y;
 
-    let text = figma.createText()
-    text.x = x;
-    text.y = y + height + 5;
+        let text = figma.createText()
+        text.x = x;
+        text.y = y + height + 5;
 
-    figma.loadFontAsync(text.fontName).then(() => {
         text.characters = hex
-    })
 
-    
-}
+      } else {
+        figma.notify('Not a rectangle with color fill')
+      }
+  }
+  figma.closePlugin()
+
+});
